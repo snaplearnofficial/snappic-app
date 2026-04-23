b8c4a004eb989677257e15bd8440013e80e921a6
snaplearnofficial <anikmondal232009@gmail.com>
2026-04-23 23:12:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -333,7 +333,7 @@ io.on('connection', (socket) => {
 });
 
 // Serve Frontend
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
